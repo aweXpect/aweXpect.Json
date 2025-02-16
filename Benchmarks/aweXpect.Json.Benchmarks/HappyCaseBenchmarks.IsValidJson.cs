5cf19a9c930d8106aad9b247ea845e2da928b577
Valentin Breuß <vbreuss@gmail.com>
2025-02-16 11:48:33 +0100

    feat: add benchmark for `IsValidJson` (#15)

---
+++ Benchmarks/aweXpect.Json.Benchmarks/HappyCaseBenchmarks.IsValidJson.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace aweXpect.Json.Benchmarks;
+
+/// <summary>
+///     In this benchmark we verify that a JSON string is valid.<br />
+/// </summary>
+public partial class HappyCaseBenchmarks
+{
+	private readonly string _validJson = """
+	                                     {
+	                                       "prop": "value",
+	                                       "prop2": 1.2,
+	                                       "prop3": false,
+	                                       "prop4": null,
+	                                       "prop5": { "nested": "value" },
+	                                       "prop6": ["an", "array", "with", "5", "items"]
+	                                     }
+	                                     """;
+
+	[Benchmark]
+	public async Task IsValidJson_aweXpect()
+		=> await Expect.That(_validJson).IsValidJson();
+}
