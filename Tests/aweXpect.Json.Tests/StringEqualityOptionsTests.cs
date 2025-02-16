6f29bf2a35843a51a3b5b4292cb07c35c6a7f40e
Valentin Breuß <vbreuss@gmail.com>
2025-02-16 20:06:16 +0100

    Fix exception in JsonElementValidator

---
+++ Tests/aweXpect.Json.Tests/StringEqualityOptionsTests.cs
@@ -0,0 +1,24 @@
+﻿using aweXpect.Core;
+using aweXpect.Options;
+using aweXpect.Results;
+
+namespace aweXpect.Json.Tests;
+
+public sealed class StringEqualityOptionsTests
+{
+	[Fact]
+	public async Task EqualJson_ShouldReturnEmptyFailure()
+	{
+		string actual = "{}";
+		string expected = "{  }";
+#pragma warning disable aweXpect0001
+		IOptionsProvider<StringEqualityOptions> optionsProvider = That(actual).IsEqualTo(expected).AsJson();
+#pragma warning restore aweXpect0001
+
+		var result = optionsProvider.Options.AreConsideredEqual(actual, expected);
+		var failure = optionsProvider.Options.GetExtendedFailure("it", actual, expected);
+
+		await That(result).IsTrue();
+		await That(failure).IsEmpty();
+	}
+}
